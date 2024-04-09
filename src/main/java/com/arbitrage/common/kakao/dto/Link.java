481f7afa64ef9ea896112dc34697fa056e554fbf
qocksdlf123 <qocksdlf123@naver.com>
2024-04-09 19:28:32 +0900

    Feat) Kakao 나에게 보내기 추가

---
+++ src/main/java/com/arbitrage/common/kakao/dto/Link.java
@@ -0,0 +1,14 @@
+package com.arbitrage.common.kakao.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Link {
+    private String web_url;
+    private String mobile_web_url;
+
+}
---
+++ src/main/java/com/arbitrage/common/kakao/dto/ReissueResponse.java
@@ -0,0 +1,10 @@
+package com.arbitrage.common.kakao.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ReissueResponse {
+    String access_token;
+
+    Integer noticeNumber;
+}
